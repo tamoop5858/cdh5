c54ed8eba80791b621cf7a6483c19e5811d27b3b
tamoop5858 <tamoop5858@users.noreply.github.com>
2017-02-04 22:56:56 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,18 @@
+apt-get install wget
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+
+apt-get install python-pip -y
+apt-get install python-apt -y
+apt-get -y install pycrypto
+python get-pip.py
+apt-get -y install python-dev
+apt-get -y install libffi-dev
+apt-get -y install apt-file
+sudo apt-file  update
+apt-get -y update && apt-get -y install libssl-dev
+pip install ansible==2.1.2.0 -I
+apt-get  install python-dev -y
+apt-get install sshpass
+apt-get install openssh-server
+
